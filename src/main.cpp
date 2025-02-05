de1890cb2e5650ac7aed3c6bfb14b63f372f1df1
Harrison Plowes <s240881@students.aie.edu.au>
2025-02-06 10:27:07 +1100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdlib>
 #include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../include/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../include/stb_image_write.h"
 
-uint8_t* Convolve(uint8_t* img, int w, int h, int chn, float* kernel, int kW, int kH);
+#define NUM_CHNS 3
+
+void Invert(uint8_t* img, int w, int h);
+void Convolve(uint8_t* in, uint8_t* out, int w, int h, float* kernel, int kW, int kH);
+void RGBtoGrayscale(uint8_t* img, int w, int h);
+void CalcGaussianKernel(float* k, int w, int h, float sigma);
 
 int main(int argc, char** argv) {
     
@@ -18,71 +25,112 @@ int main(int argc, char** argv) {
     //const char* inPath = argv[1];
     //const char* outPath = argv[2];
 
-    const char* inPath = "res/miku_original.jpg";
-    const char* outPath = "res/miku_out.png";
+    const char* inPath = "res/space.jpg";
+    const char* outPath = "res/space_out.png";
 
-    const int chn = 3;
     int w, h, comp;
-    uint8_t* img = stbi_load(inPath, &w, &h, &comp, chn);
+    uint8_t* img = stbi_load(inPath, &w, &h, &comp, NUM_CHNS);
     if (!img) {
         std::cerr << "Failed to load image.\n";
         return -1;
     }
 
-    float laplacian[] = {
-        -1, -1, -1,
-        -1,  8, -1,
-        -1, -1, -1
-    };
+    // grayscale conversion
+    RGBtoGrayscale(img, w, h);
 
-    float gaussian3[] = {
-        1 / 16.f, 1 /  8.f, 1 / 16.f, 
-        1 /  8.f, 1 /  4.f, 1 /  8.f, 
-        1 / 16.f, 1 /  8.f, 1 / 16.f,
-    };
+    { // gaussian convolve
+        const int kS = 20;
+        float k[kS * kS];
+        CalcGaussianKernel(k, kS, kS, 1.f);
+        uint8_t* tempBuf = new uint8_t[w * h * NUM_CHNS];
+        Convolve(img, tempBuf, w, h, k, kS, kS);
+        delete[] img;
+        img = tempBuf;
+    }
 
-    auto output = Convolve(img, w, h, chn, laplacian, 3, 3);
+    { // laplacian convolve
+        float k[] = { -1, -1, -1, -1,  8, -1, -1, -1, -1 };
+        uint8_t* tempBuf = new uint8_t[w * h * NUM_CHNS];
+        Convolve(img, tempBuf, w, h, k, 3, 3);
+        delete[] img;
+        img = tempBuf;
+    }
 
-    // write output buffer
-    stbi_write_png(outPath, w, h, chn, output, w * chn);
-    
-    // cleanup
+    // Invert(img, w, h);
+
+    // write output & cleanup
+    stbi_write_png(outPath, w, h, NUM_CHNS, img, w * NUM_CHNS);
     stbi_image_free(img);
-    delete[] output;
-
 	return 0;
 }
 
-uint8_t* Convolve(uint8_t* img, int w, int h, int chn, float* kernel, int kW, int kH) {
-    
-    uint8_t* output = new uint8_t[w * h * chn];
-    int dx = kW / 2, dy = kH / 2;
-
-    // iterate pxls in img
+void Invert(uint8_t* img, int w, int h) {
     for (int y = 0; y < h; y++) {
         for (int x = 0; x < w; x++) {
-
-            // iterate channels
-            for (int c = 0; c < chn; c++) {
-
-                float sum = 0.f;
-
-                // iterate over kernel, convolving kernel with img into sum
-                for (int ky = -dy; ky <= dy; ky++) {
-                    for (int kx = -dx; kx <= dx; kx++) {
-
-                        int ny = std::min(std::max(y + ky, 0), h - 1);
-                        int nx = std::min(std::max(x + kx, 0), w - 1);
-
-                        sum += img[chn * (ny * w + nx) + c] * kernel[(ky + dy) * kW + (kx + dx)];
-                    }
-                }
-
-                // set val in output buf to sum, for current pixel and channel
-                output[chn * (y * w + x) + c] = std::min(std::max((int)sum, 0), 255);
+            for (int c = 0; c < 3; c++) {
+                img[NUM_CHNS * (y * w + x) + c] = 255 - img[NUM_CHNS * (y * w + x) + c];
             }
         }
     }
+}
 
-    return output;
+void RGBtoGrayscale(uint8_t* img, int w, int h) {
+    // iterate pxls in img
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            // grayscale conversion values
+            float rMod = 0.299, gMod = 0.587, bMod = 0.114;
+
+            // calculate vals for each channel
+            float rt = ((float)img[NUM_CHNS * (y * w + x)] / 255.f) * rMod;
+            float gt = ((float)img[NUM_CHNS * (y * w + x) + 1] / 255.f) * gMod;
+            float bt = ((float)img[NUM_CHNS * (y * w + x) + 2] / 255.f) * bMod;
+
+            // sum calculated values and write to current pixel
+            uint8_t fVal = (uint8_t)std::min(std::max((int)((rt + gt + bt) * 255), 0), 255);
+            for (int c = 0; c < NUM_CHNS; c++) img[NUM_CHNS * (y * w + x) + c] = fVal;
+        }
+    }
+}
+
+void Convolve(uint8_t* in, uint8_t* out, int w, int h, float* kernel, int kW, int kH) {
+    int dx = kW / 2, dy = kH / 2;
+    // iterate pxls in img
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            // iterate channels
+            for (int c = 0; c < NUM_CHNS; c++) {
+                float sum = 0.f;
+                // iterate over kernel, convolving kernel with img into sum
+                for (int ky = -dy; ky <= dy; ky++) {
+                    for (int kx = -dx; kx <= dx; kx++) {
+                        int ny = std::min(std::max(y + ky, 0), h - 1);
+                        int nx = std::min(std::max(x + kx, 0), w - 1);
+                        sum += in[NUM_CHNS * (ny * w + nx) + c] * kernel[(ky + dy) * kW + (kx + dx)];
+                    }
+                }
+                // set val in output buf to sum, for current pixel and channel
+                out[NUM_CHNS * (y * w + x) + c] = std::min(std::max((int)sum, 0), 255);
+            }
+        }
+    }
+}
+
+void CalcGaussianKernel(float* k, int w, int h, float sigma) {
+    int dx = w / 2, dy = h / 2;
+    float s = 2.f * sigma * sigma, sum = 0.f;
+    // generate kernel
+    for (int y = -dy; y <= dy; y++) {
+        for (int x = -dx; x <= dx; x++) {
+            float r = sqrt(x * x + y * y);
+            k[(y + dy) * w + (x + dx)] = (exp(-(r * r) / s)) / (M_PI * s);
+            sum += k[(y + dy) * w + (x + dx)];
+        }
+    }
+    // normalise kernel
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            k[y * w + x] /= sum;
+        }
+    }
 }

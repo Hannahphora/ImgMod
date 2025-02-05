e4f56be8f38500f9333c52e4dbd8a1278881d713
Harrison Plowes <s240881@students.aie.edu.au>
2025-02-05 16:42:02 +1100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <cstdlib>
 #include <iostream>
-#include <algorithm>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../include/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../include/stb_image_write.h"
 
+void Convolve();
+
 int main(int argC, char* argV[]) {
     
-    // TODO: implement taking input file from cmdline arguments
+    // TODO: implement taking input img path from cmdline arguments
     // if (argC <= 1) std::cout << "Error: No input image specified\n";
 
     const int CHN = 3;
@@ -19,22 +20,36 @@ int main(int argC, char* argV[]) {
     int w, h, comp;
     uint8_t* img = stbi_load(inPath, &w, &h, &comp, CHN);
 
+    // FORMULA FOR FLAT 2D ARRAY ACCESS:
+    // array[y * width + x]
+
+    const int kW = 3, kH = 3;
+    const int kernel[] = {
+        -1, -1, -1,
+        -1,  8, -1,
+        -1, -1, -1
+    };
+
+    uint8_t* window[kW * kH];
+    int dx = kW / 2, dy = kH / 2;
+
+    for (int y = -dy; y <= dy; y++) {
+        for (int x = -dx; x <= dx; x++) {
+            window[y * kW + x] = img;
+        }
+    }
+
     for (int y = 0; y < h; y++) {
         for (int x = 0; x < w; x++) {
-            // convert values
-            int r = img[CHN * (y * w + x)];
-            int g = img[CHN * (y * w + x) + 1];
-            int b = img[CHN * (y * w + x) + 2];
+            img[CHN * (y * w + x)];
+            img[CHN * (y * w + x) + 1];
+            img[CHN * (y * w + x) + 2];
 
 
-
-            // convert values back
-            img[CHN * (y * w + x)] = (uint8_t)r;
-            img[CHN * (y * w + x) + 1] = (uint8_t)g;
-            img[CHN * (y * w + x) + 2] = (uint8_t)b;
         }
     }
 
     stbi_write_png(outPath, w, h, CHN, img, w * CHN);
+    stbi_image_free(img);
 	return 0;
 }

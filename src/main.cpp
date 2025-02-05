cecd921254ea28f895e55a33467d0044a07e8359
Harrison Plowes <s240881@students.aie.edu.au>
2025-02-05 15:45:33 +1100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <algorithm>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../include/stb_image.h"
@@ -7,21 +8,33 @@
 #include "../include/stb_image_write.h"
 
 int main(int argC, char* argV[]) {
+    
+    // TODO: implement taking input file from cmdline arguments
+    // if (argC <= 1) std::cout << "Error: No input image specified\n";
+
+    const int CHN = 3;
+    const char* inPath = "res/input/test.jpg";
+    const char* outPath = "res/output/test.png";
+
+    int w, h, comp;
+    uint8_t* img = stbi_load(inPath, &w, &h, &comp, CHN);
+
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            // convert values
+            int r = img[CHN * (y * w + x)];
+            int g = img[CHN * (y * w + x) + 1];
+            int b = img[CHN * (y * w + x) + 2];
 
 
 
-    uint8_t* img = (uint8_t*)new char[160 * 60 * 3];
-
-    for (int y = 0; y < 60; y++) {
-        for (int x = 0; x < 160; x++) {
-            img[3 * (y * 160 + x)] = 128;
-            img[3 * (y * 160 + x) + 1] = 128;
-            img[3 * (y * 160 + x) + 2] = 128;
+            // convert values back
+            img[CHN * (y * w + x)] = (uint8_t)r;
+            img[CHN * (y * w + x) + 1] = (uint8_t)g;
+            img[CHN * (y * w + x) + 2] = (uint8_t)b;
         }
     }
 
-    // stbi_write_png(path, width, height, CHANNEL_NUM, pixels, width * CHANNEL_NUM);
-    stbi_write_png("res/output/test.png", 160, 60, 3, img, 160 * 3);
-
+    stbi_write_png(outPath, w, h, CHN, img, w * CHN);
 	return 0;
 }

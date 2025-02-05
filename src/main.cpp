52f3496c48e0f212e3788ff18ba4c687ffafc031
Harrison Plowes <harrisonplowes958@gmail.com>
2025-02-05 19:09:11 +1100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,51 +6,93 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../include/stb_image_write.h"
 
-void Convolve();
+uint8_t* Convolve(uint8_t* img, int w, int h, int chn, float* kernel, int kW, int kH);
 
-int main(int argC, char* argV[]) {
+int main(int argc, char** argv) {
     
-    // TODO: implement taking input img path from cmdline arguments
-    // if (argC <= 1) std::cout << "Error: No input image specified\n";
+    //if (argc < 3) {
+    //    std::cerr << "Usage: " << argv[0] << " <input> <output>\n";
+    //    return 1;
+    //}
+    //const char* inPath = argv[1];
+    //const char* outPath = argv[2];
 
-    const int CHN = 3;
-    const char* inPath = "res/input/test.jpg";
-    const char* outPath = "res/output/test.png";
+    const char* inPath = "res/miku_original.jpg";
+    const char* outPath = "res/miku_out.png";
 
+    const int chn = 3;
     int w, h, comp;
-    uint8_t* img = stbi_load(inPath, &w, &h, &comp, CHN);
+    uint8_t* img = stbi_load(inPath, &w, &h, &comp, chn);
+    if (!img) {
+        std::cerr << "Failed to load image.\n";
+        return -1;
+    }
 
     // FORMULA FOR FLAT 2D ARRAY ACCESS:
     // array[y * width + x]
 
-    const int kW = 3, kH = 3;
-    const int kernel[] = {
+    float laplacian[] = {
         -1, -1, -1,
         -1,  8, -1,
         -1, -1, -1
     };
 
-    // TODO: replace wraparound edge handling with extend
+    float gaussian3[] = {
+        1 / 16.f, 1 /  8.f, 1 / 16.f, 
+        1 /  8.f, 1 /  4.f, 1 /  8.f, 
+        1 / 16.f, 1 /  8.f, 1 / 16.f,
+    };
 
-    uint8_t* window[kW * kH];
+    float gaussian5[] = {
+        0.00292, 0.01306, 0.02154, 0.01306, 0.00292,
+        0.01306, 0.05855, 0.09653, 0.05855, 0.01306,
+        0.02154, 0.09653, 0.15915, 0.09653, 0.02154,
+        0.01306, 0.05855, 0.09653, 0.05855, 0.01306,
+        0.00292, 0.01306, 0.02154, 0.01306, 0.00292
+    };
+
+    auto output = Convolve(img, w, h, chn, laplacian, 3, 3);
+
+    // write output buffer
+    stbi_write_png(outPath, w, h, chn, output, w * chn);
+    
+    // cleanup
+    stbi_image_free(img);
+    delete[] output;
+
+	return 0;
+}
+
+uint8_t* Convolve(uint8_t* img, int w, int h, int chn, float* kernel, int kW, int kH) {
+    
+    uint8_t* output = new uint8_t[w * h * chn];
     int dx = kW / 2, dy = kH / 2;
-    for (int y = -dy; y <= dy; y++) {
-        for (int x = -dx; x <= dx; x++) {
-            window[(y + dy) * kW + (x + dy)] = img + ((y % h) * w + (x % w));
-        }
-    }
 
+    // iterate pxls in img
     for (int y = 0; y < h; y++) {
         for (int x = 0; x < w; x++) {
-            img[CHN * (y * w + x)];
-            img[CHN * (y * w + x) + 1];
-            img[CHN * (y * w + x) + 2];
 
+            // iterate channels
+            for (int c = 0; c < chn; c++) {
 
+                float sum = 0.f;
+
+                // iterate over kernel, calculating sum
+                for (int ky = -dy; ky <= dy; ky++) {
+                    for (int kx = -dx; kx <= dx; kx++) {
+
+                        int ny = std::min(std::max(y + ky, 0), h - 1);
+                        int nx = std::min(std::max(x + kx, 0), w - 1);
+
+                        sum += img[chn * (ny * w + nx) + c] * kernel[(ky + dy) * kW + (kx + dx)];
+                    }
+                }
+
+                // set val in output buf, for current pixel and channel
+                output[chn * (y * w + x) + c] = std::min(std::max(int(sum), 0), 255);
+            }
         }
     }
 
-    stbi_write_png(outPath, w, h, CHN, img, w * CHN);
-    stbi_image_free(img);
-	return 0;
+    return output;
 }
